3d9c68366e6c4177c2b29e695840b64d0b768cbd
Masashi <etaonrish13@gmail.com>
2016-03-19 06:57:03 +0000

    modified routes and group-making page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 #  root to: "groups#index"
   get'/login' => 'groups#login'
 
+  root to: "groups#index"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
